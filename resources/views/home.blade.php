b7383e79c812e7b9970de3089dd0e8e58decda71
Mark Lester Morta <marklester10.mlsm@gmail.com>
2020-11-18 17:49:02 +0800

    First Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
    <div class="container mx-auto py-6">
     <div class="flex justify-center mx-auto">
         <div class="w-11/12 md:w-4/5 bg-white p-6 rounded-lg">
-            <img class="mx-auto" src="{{ asset('imgs/posty.png') }}" alt="Logo">
-            <p class="py-3">
+            <img class="w-24 mx-auto" src="{{ asset('imgs/posty.png') }}" alt="Logo">
+            <p class="py-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, beatae minus! Debitis fugit doloribus cum! Facilis quo numquam deleniti officiis eos aperiam culpa voluptatem sed qui voluptatum rerum ad accusantium, iure, fuga nesciunt temporibus expedita, molestias autem maxime accusamus voluptatibus error ipsum odit molestiae. Nam veniam at facere nesciunt harum.
             </p>
-            <p class="py-3">
+            <p class="py-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, beatae minus! Debitis fugit doloribus cum! Facilis quo numquam deleniti officiis eos aperiam culpa voluptatem sed qui voluptatum rerum ad accusantium, iure, fuga nesciunt temporibus expedita, molestias autem maxime accusamus voluptatibus error ipsum odit molestiae. Nam veniam at facere nesciunt harum. accusantium, iure, fuga nesciunt temporibus expedita, molestias autem maxime accusamus voluptatibus error ipsum odit molestiae. Nam veniam at facere nesciunt harum.
             </p>
         </div>
